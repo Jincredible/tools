6c5e2beffe0b907b3157648947b5bc79f3de4934
Steven Jin <steven.jin.dartmouth@gmail.com>
2021-06-16 17:48:45 -0400

    Add write functions and other helper functions to file_interface

--- file_interface/file_interface.py
+++ file_interface/file_interface.py
@@ -6,14 +6,22 @@ import csv
 class FileInterface(object):
     """Reads and writes to files using lists of namedtuples"""
 
-    def __init__(self, arg):
+    def __init__(self):
         return None
 
 
     @staticmethod
-    def overwrite_to_file(data:typing.List[typing.NamedTuple],filename:str) -> None:
+    def overwrite_list_of_dicts_to_file(data:typing.List[dict],filename:str) -> None:
+        if not FileInterface.does_filename_exist(filename=filename):
+            FileInterface.create_file(filename=filename)
+        
+        FileInterface.truncate_file(filename=filename)
+        FileInterface.write_list_of_dicts_to_file(filename=filename)
         return None
 
+    @staticmethod
+    def write_list_of_dicts_to_file(data:typing.List[dict],filename:str)->None:
+        return None
 
     @staticmethod
     def does_filename_exist(filename:str) -> bool:
@@ -21,21 +29,33 @@ class FileInterface(object):
 
 
     @staticmethod
-    def delete_file(filename:str) -> None:
+    def is_file_empty(filename:str) -> bool:
+        return os.stat(filename).st_size == 0
+
+
+    @staticmethod
+    def truncate_file(filename:str) -> None:
+        with open(file=filename, mode='r', newline=None) as csv_file:
+            csv_file.truncate()
         return None
 
 
     @staticmethod
     def create_file(filename:str) -> None:
+        with open(filename, 'w') as fp:
+            pass
         return None
 
     
     @staticmethod
-    def convert_file_to_list_of_tuples(filename:str) -> typing.List[typing.NamedTuple]:
+    def convert_file_to_list_of_dicts(filename:str) -> typing.List[typing.NamedTuple]:
+        if not FileInterface.does_filename_exist(filename=filename) or FileInterface.is_file_empty(filename=filename):
+            return None
 
         with open(file=filename, mode='r', newline=None) as csv_file:
-            reader = csv.DictReader(csv_file)
-        return None
+            reader:csv.DictReader = csv.DictReader(csv_file)
+
+        return [row for row in reader]
 
 
 
