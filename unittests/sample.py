770fd06f89d0e46172c5ab5f9c7705aa4aa3a2d0
Steven Jin <steven.jin.dartmouth@gmail.com>
2021-06-16 23:10:13 -0400

    Add test for writing and reading from files. Also add tests for exceptions

--- unittests/sample.py
+++ unittests/sample.py
@@ -1,11 +1,11 @@
-
-
+import os, sys
 
 def function_return_list_length(input_list:list) -> int:
     """purpose: example of a function with no console or side effects
     input: list object
     output: int
     side effects: none"""
+    
     return len(input_list)
 
 
@@ -14,6 +14,7 @@ def function_with_one_console_input()->int:
     input: console only, no parameters
     output: int
     side effects: none"""
+    
     return len(input('input anything on console: '))
 
 
@@ -22,6 +23,7 @@ def function_with_multiple_console_inputs()->float:
     input: console only, no parameters
     output: float
     side effects: none"""
+    
     input_str_1: str = input('input #1... enter a string: ')
     input_int_2: int = int(input('input #2... enter an int: '))
     input_float_3: float = float(input('input #3... enter a float: '))
@@ -36,6 +38,7 @@ def function_with_print(input_str:str) -> None:
     side effects: a print to console one time
     important note: when the input is None, we want to use the logical gate {input_str or ''} to print
         an empty string"""
+    
     print(f"hi {input_str or ''}")
     return None
 
@@ -55,7 +58,7 @@ def function_with_multiple_prints(input_str: str) -> None:
         num_letters: int = len(input_str)
     except TypeError:
         num_letters: int = 0
-    
+
     print(f"you have {num_letters} letters in your name")
     return None
 
@@ -68,10 +71,51 @@ def function_read_and_write_to_console_once() -> None:
     side effects: a print to console one time
     important note: when the input is None, we want to use the logical gate {input_str or ''} to print
         an empty string"""
+
     print(f"hi {input('Enter your name: ') or ''}")
     return None
 
 
 
+def function_that_reads_from_a_file(filename:str)->list:
+    """purpose: example of a function that reads text from a file and outputs to a list
+    input: a filename and a file that exists in the path
+    output: a list
+    side effects: None"""
+
+    file_data: list = list()
+    with open(filename, mode='r') as file_wrapper:
+        for row in file_wrapper:
+            file_data.append(row.rstrip())
+    return file_data
+
+
+def function_that_writes_to_a_file(filename:str, data:list) -> None:
+    """purpose: example of a function that writes data to a file
+    input: filename and data to write
+    output: None
+    side effects: writes text to a file"""
+
+    with open(filename, mode='w') as file_wrapper:
+        for row in data:
+            file_wrapper.write((row or '') +"\n") # (row or '') allows for Nones
+    return None
+
+
+def function_that_raises_an_exception(input_str:str) -> None:
+    """purpose: example of a function that raises an exception
+    input: input_str
+    output: int, length of string
+    side effects: None"""
+    return len(input_str)
+
+
 if __name__ == '__main__':
     pass
+
+
+
+
+
+
+
--- unittests/tests/test_sample.py
+++ unittests/tests/test_sample.py
@@ -2,6 +2,7 @@ import sys
 import os
 import typing
 import pathlib
+import io
 path_list = str(pathlib.Path(__file__)).split("/")[1:-1][::-1] # take out the first and last element and reverse list
 
 path_to_code: str = str(pathlib.Path(__file__).parents[path_list.index('unittests')])
@@ -65,18 +66,12 @@ class TestReturnListLength(unittest.TestCase):
 
 
     def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
+        """This module is executed last to clean up after unit tests"""
         return None
-
 
 
 @patch('builtins.input')
 class TestConsoleInput(unittest.TestCase):
-
-
-    def setUp(self):
-        """This module is executed first before anything else, to prepare for our unit tests"""
-        return None
 
 
     def test__function_with_one_console_input__simple_input(self, mock_input):
@@ -103,19 +98,9 @@ class TestConsoleInput(unittest.TestCase):
         return None
 
 
-    def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
-        return None
-
-
 
 @patch('builtins.input')
 class TestMultipleConsoleInput(unittest.TestCase):
-
-
-    def setUp(self):
-        """This module is executed first before anything else, to prepare for our unit tests"""
-        return None
 
 
     def test__function_with_multiple_console_inputs__simple_input(self, mock_input):
@@ -165,18 +150,11 @@ class TestMultipleConsoleInput(unittest.TestCase):
 
         return None
 
-    def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
-        return None
-
 
 
 @patch('builtins.print')
 class TestPrintToConsole(unittest.TestCase):
 
-    def setUp(self):
-        """This module is executed first before anything else, to prepare for our unit tests"""
-        return None
 
 
     def test__function_with_print__empty_input(self, mock_print):
@@ -203,11 +181,6 @@ class TestPrintToConsole(unittest.TestCase):
         mock_print.assert_called_with(expected_output)
 
 
-    def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
-        return None
-
-
 
 @patch('builtins.print')
 class TestPrintToConsoleMultipleTimes(unittest.TestCase):
@@ -219,39 +192,34 @@ class TestPrintToConsoleMultipleTimes(unittest.TestCase):
 
     def test__function_with_multiple_prints__empty_input(self, mock_print):
         input_str: str = ''
-        expected_output: list = [call('hi '), call('nice to meet you '), call('you have 0 letters in your name')]
+        expected_calls: list = [call('hi '), call('nice to meet you '), call('you have 0 letters in your name')]
         sample.function_with_multiple_prints(input_str=input_str)
-        mock_print.assert_has_calls(expected_output)
+        mock_print.assert_has_calls(expected_calls)
 
 
     def test__function_with_multiple_prints__normal_input(self, mock_print):
         input_str: str = 'Steven'
-        expected_output: list = [call('hi Steven'), call('nice to meet you Steven'), call('you have 6 letters in your name')]
+        expected_calls: list = [call('hi Steven'), call('nice to meet you Steven'), call('you have 6 letters in your name')]
         sample.function_with_multiple_prints(input_str=input_str)
-        mock_print.assert_has_calls(expected_output)
+        mock_print.assert_has_calls(expected_calls)
 
 
     def test__function_with_multiple_prints__none_input(self, mock_print):
         input_str: str = None
-        expected_output: list = [call('hi '), call('nice to meet you '), call('you have 0 letters in your name')]
+        expected_calls: list = [call('hi '), call('nice to meet you '), call('you have 0 letters in your name')]
         sample.function_with_multiple_prints(input_str=input_str)
-        mock_print.assert_has_calls(expected_output)
+        mock_print.assert_has_calls(expected_calls)
 
     def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
+        """This module is executed last to clean up after unit tests"""
         return None
 
 
 
-
-@patch('builtins.print')
+@patch('builtins.print') # the order of this is really important!
 @patch('builtins.input')
 class TestPrintReadAndWriteToConsole(unittest.TestCase):
 
-
-    def setUp(self):
-        """This module is executed first before anything else, to prepare for our unit tests"""
-        return None
 
 
     def test__function_read_and_write_to_console_once__normal_input(self, mock_input, mock_print):
@@ -272,9 +240,100 @@ class TestPrintReadAndWriteToConsole(unittest.TestCase):
         mock_print.assert_called_with(expected_output)
 
 
-    def tearDown(self):
-        """This module is executed last to prepare for our unit tests"""
-        return None
+
+@patch('builtins.open')
+class TestReadFromFile(unittest.TestCase):
+
+
+    def test__function_that_reads_from_a_file__normal_file_contents(self, mock_open):
+        
+        file_data:io.StringIO = io.StringIO('first line\nsecond line\n\n123\nlast line')
+        mock_open.return_value = file_data
+        filename: str = 'path/to/open/file.txt'
+
+
+        expected_output:list = ['first line', 'second line', '', '123', 'last line']
+        actual_output: list = sample.function_that_reads_from_a_file(filename=filename)
+
+
+        self.assertEqual(first=actual_output, second=expected_output)
+        mock_open.assert_called_with(filename, mode='r')
+
+
+    def test__function_that_reads_from_a_file_once__empty_input(self, mock_open):
+        
+        file_data:io.StringIO = io.StringIO('')
+        mock_open.return_value = file_data
+        filename: str = 'path/to/open/file.txt'
+
+
+        expected_output:list = []
+        actual_output: list = sample.function_that_reads_from_a_file(filename=filename)
+
+
+        self.assertEqual(first=actual_output, second=expected_output)
+        mock_open.assert_called_with(filename, mode='r')
+
+
+    def test__function_that_reads_from_a_file_once__no_input(self, mock_open):
+        
+        file_data:io.StringIO = io.StringIO(None)
+        mock_open.return_value = file_data
+        filename: str = 'path/to/open/file.txt'
+
+
+        expected_output:list = []
+        actual_output: list = sample.function_that_reads_from_a_file(filename=filename)
+
+
+        self.assertEqual(first=actual_output, second=expected_output)
+        mock_open.assert_called_with(filename, mode='r')
+
+
+
+@patch('builtins.open')
+class TestWriteToFile(unittest.TestCase):
+
+
+    def test__function_that_writes_to_a_file__normal_data(self, mock_open):
+        
+        file_data:list = ['!!123', 'test row 1', '', None, 'last row']
+        filename: str = 'path/to/open/file.txt'
+
+        expected_calls:list = [call('!!123\n'), call('test row 1\n'), call('\n'), call('\n'), call('last row\n')]
+
+        sample.function_that_writes_to_a_file(filename=filename, data=file_data)
+
+        mock_open.assert_called_with(filename, mode='w')
+        mock_open().__enter__().write.assert_has_calls(expected_calls)
+
+
+
+    def test__function_that_writes_to_a_file__empty_input(self, mock_open):
+        
+        file_data:list = [None]
+        filename: str = 'path/to/open/file.txt'
+
+        expected_calls:list = [call('\n')]
+
+        sample.function_that_writes_to_a_file(filename=filename, data=file_data)
+
+        mock_open.assert_called_with(filename, mode='w')
+        mock_open().__enter__().write.assert_has_calls(expected_calls)
+
+
+class TestRaiseException(unittest.TestCase):
+
+    def test__function_that_raises_an_exception__none_input(self):
+        input_str: None = None
+        self.assertRaises(TypeError, sample.function_that_raises_an_exception, input_str)
+
+
+    def test__function_that_raises_an_exception__num_input(self):
+        input_str: int = 0
+        self.assertRaises(TypeError, sample.function_that_raises_an_exception, input_str)
+
+
 
 
 if __name__ == '__main__':
